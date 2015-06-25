b10970ae82bc6bb3eb6d32e5312323497ec28c10
Bernhard <berni98k@gmail.com>
2015-06-25 19:21:43 +0200

    asyncron regex testing, generating regex objects at the beginning

--- regexAnswers.js
+++ regexAnswers.js
@@ -1,16 +1,21 @@
+var async = require('async');
+
 var telegram = require('./telegram.js');
 var conf = require('./regexConf.json');
 
-exports.process = function(message){
-	conf.answers.forEach(function(answer){
-		var regexp = new RegExp(answer.regex, 'i');
+conf.answers.forEach(function(answer){
+	answer.regexObj = regexp = new RegExp(answer.regex, answer.flag);
+});
 
-		if(regexp.test(message.text)){
+exports.process = function(message){
+	async.each(conf.answers, function(answer, callback){
+		if(answer.regexObj.test(message.text)){
 			telegram.api({
 				method: 'sendMessage',
 				chat_id: message.chat.id,
 				text: answer.answer
 			});
 		}
+		callback();
 	});
 };
