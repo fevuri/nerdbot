9e006de443e7c3a4d7e60acd29f128f43e98bcdd
Bernhard <berni98k@gmail.com>
2015-07-05 18:17:15 +0200

    a few comments

--- app.js
+++ app.js
@@ -9,17 +9,21 @@ var modules = {
 var apikeys = require('./apikeys.json');
 var config = require('./config.json');
 
-
+// Start the different Bots
 async.each(config.bots, function(botConfig){
 	telegram.init(apikeys[botConfig.name], botConfig, function(bot){
+		// Initialize every module of the bot
 		async.each(bot.config.activeModules, function(moduleName, done){
 			if(typeof modules[moduleName].init == 'function'){
 				modules[moduleName].init(bot, done);
 			}else{
 				done();
 			}
-		}, function(){
+		},
+		// When the initialization of the modules is finished, set an onMessage function
+		function(){
 			bot.onMessage(function(message){
+				// If a message is received, pass it to all activeated modules
 				async.each(bot.config.activeModules, function(moduleName){
 					if(typeof modules[moduleName].process == 'function'){
 						modules[moduleName].process(bot, message);
