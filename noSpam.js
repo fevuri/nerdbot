30c7f30420c897295bea269ccf60ad5aebafd559
Bernhard <berni98k@gmail.com>
2015-07-05 19:57:31 +0200

    also show "kein spam bitte" if the same text is recieved multiple times

--- noSpam.js
+++ noSpam.js
@@ -1,3 +1,5 @@
+var async = require('async');
+
 exports.init = function(bot, done){
 	var noSpam = bot.config.noSpam;
 	done();
@@ -10,23 +12,48 @@ exports.process = function(bot, message){
 	if(!noSpam.lastMessages[message.chat.id]) noSpam.lastMessages[message.chat.id] = new ChatHistory();
 	function ChatHistory(){
 		this.timestamps = [];
+		this.lastText = {
+			text: "",
+			count: 0,
+		}
 	}
 	var lastMessagesInChat = noSpam.lastMessages[message.chat.id]; // helper variable
 
-	// answer if there are to many replys in a minute
-	if(noSpam.minuteLimit){
-		var now = new Date();
-		lastMessagesInChat.timestamps.push(now);
-		lastMessagesInChat.timestamps = lastMessagesInChat.timestamps.filter(function(timestamp){
-			return now.getTime() - timestamp.getTime() < 60*1000;
-		});
-		if(lastMessagesInChat.timestamps.length > noSpam.minuteLimit){
-			bot.api({
-				method: 'sendMessage',
-				chat_id: message.chat.id,
-				text: noSpam.reply
-			});
+	// async.some to check if one of the different spam filters is true
+	async.some([
+		// too many replys in a minute
+		function(){
+			if(noSpam.minuteLimit){
+				var now = new Date();
+				lastMessagesInChat.timestamps.push(now);
+				lastMessagesInChat.timestamps = lastMessagesInChat.timestamps.filter(function(timestamp){
+					return now.getTime() - timestamp.getTime() < 60*1000;
+				});
+				if(lastMessagesInChat.timestamps.length > noSpam.minuteLimit) return true;
+			}
+			return false;
+		},
+		// too often the same content
+		function(){
+			if(message.text == lastMessagesInChat.lastText.text){
+				lastMessagesInChat.lastText.count++;
+				if(lastMessagesInChat.lastText.count > noSpam.sameTextLimit) return true;
+			}else{
+				lastMessagesInChat.lastText.text = message.text;
+				lastMessagesInChat.lastText.count = 1;
+			}
+			return false;
+		}],
+		function(func, callback){callback(func())}, // mini function to call the functions of the array and pass their result to callback
+		function(reuslt){
+			// answer if one of the spam filters are true
+			if(reuslt){
+				bot.api({
+					method: 'sendMessage',
+					chat_id: message.chat.id,
+					text: noSpam.reply
+				});
+			}
 		}
-	}
-	//TODO limit messages with same text, maybe some kind of asyc implementation
+	);
 };
