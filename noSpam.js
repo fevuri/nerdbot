586cda3488b1bb79b4c32c09367ca207bc11e9e8
Bernhard <berni98k@gmail.com>
2015-07-05 19:30:03 +0200

    reply "kein Spam bitte" after 4 messages in a minute with NoSpamBot

--- noSpam.js
+++ noSpam.js
@@ -1,3 +1,33 @@
-exports.process = function(bot, message){
-    //TODO
+exports.init = function(bot, done){
+	var noSpam = bot.config.noSpam;
+	done();
 };
+
+exports.process = function(bot, message){
+	var noSpam = bot.config.noSpam;
+	if(!noSpam.lastMessages){
+		noSpam.lastMessages = {};
+	}
+	if(!noSpam.lastMessages[message.chat.id]){
+		noSpam.lastMessages[message.chat.id] = new ChatHistory();
+	}
+	var lastMessagesInChat = noSpam.lastMessages[message.chat.id];
+	if(noSpam.minuteLimit){
+		var now = new Date();
+		lastMessagesInChat.timestamps.push(now);
+		lastMessagesInChat.timestamps = lastMessagesInChat.timestamps.filter(function(timestamp){
+			return now.getTime() - timestamp.getTime() < 60*1000;
+		});
+		if(lastMessagesInChat.timestamps.length > noSpam.minuteLimit){
+			bot.api({
+				method: 'sendMessage',
+				chat_id: message.chat.id,
+				text: noSpam.reply
+			});
+		}
+	}
+};
+
+function ChatHistory(){
+	this.timestamps = [];
+}
