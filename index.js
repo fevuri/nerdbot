1e818d998ccdd26694bbc5cc8346ff9d87fe9f14
Bernhard <berni98k@gmail.com>
2015-07-12 17:15:22 +0200

    automatically load botmodules from folder

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 require('console-stamp')(console, 'yyyy-mm-dd HH:MM:ss.l');
 
 var async = require('async');
+var fs = require('fs');
+var path = require('path');
 
 var telegram = require('./lib/telegram.js');
-var modules = {
-	regexAnswers: require('./lib/regexAnswers.js'),
-	noSpam: require('./lib/noSpam.js'),
-	forwarder: require('./lib/forwarder.js')
-};
 
 var apikeys = require('./apikeys.json');
 var config = require('./config.json');
+
+// Load all existing botmodules
+var botmodules = {};
+fs.readdirSync('botmodules').forEach(function(modulefilename){
+	var modulepath = path.join('botmodules', modulefilename);
+	if(path.extname(modulepath) == '.js'){
+		var modulename = path.basename(modulepath, '.js');
+		botmodules[modulename] = require(path.resolve(modulepath));
+		console.log("Loaded botmodule " + modulename);
+	}
+	// TODO: handle folder as botmodules
+});
 
 // Start the different Bots
 async.each(config.bots, function(botConfig){
 	bot = new telegram(apikeys[botConfig.username], botConfig, function(bot){
 		// Initialize every module of the bot
 		async.each(Object.keys(bot.config.modules), function(moduleName, done){
-			if(typeof modules[moduleName].init == 'function'){
-				modules[moduleName].init(bot, done);
+			if(typeof botmodules[moduleName].init == 'function'){
+				botmodules[moduleName].init(bot, done);
 			}else{
 				done();
 			}
@@ -29,8 +38,8 @@ async.each(config.bots, function(botConfig){
 			bot.on('message', function(msg){
 				// If a message is received, pass it to all activeated modules
 				async.each(Object.keys(bot.config.modules), function(moduleName){
-					if(typeof modules[moduleName].process == 'function'){
-						modules[moduleName].process(bot, msg);
+					if(typeof botmodules[moduleName].process == 'function'){
+						botmodules[moduleName].process(bot, msg);
 					}
 				});
 			})
