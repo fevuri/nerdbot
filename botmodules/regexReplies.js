1c90e09ecc9d4de10311a446ae21143c67e1a26f
Bernhard <berni98k@gmail.com>
2015-09-07 21:16:47 +0200

    refactor: removed useless variable

--- botmodules/regexReplies.js
+++ botmodules/regexReplies.js
@@ -3,7 +3,7 @@ var async = require('async');
 // Create the RegExp objects for all the regular expressions
 exports.init = function(bot, done) {
 	async.each(bot.config.modules.regexReplies, function(reply, done){
-		reply.regexObj = regexp = new RegExp(reply.regex, reply.flag);
+		reply.regexObj = new RegExp(reply.regex, reply.flag);
 		done();
 	}, done);
 }
