cf5193fbf7324fe18a25dca94094f976617b4467
Bernhard <berni98k@gmail.com>
2015-09-12 17:45:14 +0200

    refractor: used atom-beautyfy on the project

--- botmodules/noSpam.js
+++ botmodules/noSpam.js
@@ -1,11 +1,12 @@
 var async = require('async');
 
-exports.process = function(bot, message){
+exports.process = function (bot, message) {
 	var noSpam = bot.config.modules.noSpam; // helper variable
 	// create the lastMessages and ChatHistory objects for the chat if they dont exist
 	if(!noSpam.lastMessages) noSpam.lastMessages = {};
 	if(!noSpam.lastMessages[message.chat.id]) noSpam.lastMessages[message.chat.id] = new ChatHistory();
-	function ChatHistory(){
+
+	function ChatHistory() {
 		this.timestamps = [];
 		this.lastText = {
 			text: "",
@@ -16,38 +17,41 @@ exports.process = function(bot, message){
 
 	// async.some to check if one of the different spam filters is true
 	async.some([
-		// too many replys in a minute
-		function(){
-			if(noSpam.minuteLimit){
-				var now = new Date();
-				lastMessagesInChat.timestamps.push(now);
-				lastMessagesInChat.timestamps = lastMessagesInChat.timestamps.filter(function(timestamp){
-					return now.getTime() - timestamp.getTime() < 60*1000;
-				});
-				if(lastMessagesInChat.timestamps.length > noSpam.minuteLimit) return true;
-			}
-			return false;
-		},
-		// too often the same content
-		function(){
-			if(message.text){
-				if(message.text == lastMessagesInChat.lastText.text){
-					lastMessagesInChat.lastText.count++;
-					if(lastMessagesInChat.lastText.count > noSpam.sameTextLimit) return true;
-				}else{
-					lastMessagesInChat.lastText.text = message.text;
+			// too many replys in a minute
+			function () {
+				if(noSpam.minuteLimit) {
+					var now = new Date();
+					lastMessagesInChat.timestamps.push(now);
+					lastMessagesInChat.timestamps = lastMessagesInChat.timestamps.filter(function (timestamp) {
+						return now.getTime() - timestamp.getTime() < 60 * 1000;
+					});
+					if(lastMessagesInChat.timestamps.length > noSpam.minuteLimit) return true;
+				}
+				return false;
+			},
+			// too often the same content
+			function () {
+				if(message.text) {
+					if(message.text == lastMessagesInChat.lastText.text) {
+						lastMessagesInChat.lastText.count++;
+						if(lastMessagesInChat.lastText.count > noSpam.sameTextLimit) return true;
+					} else {
+						lastMessagesInChat.lastText.text = message.text;
+						lastMessagesInChat.lastText.count = 1;
+					}
+				} else {
+					lastMessagesInChat.lastText.text = "";
 					lastMessagesInChat.lastText.count = 1;
 				}
-			}else{
-				lastMessagesInChat.lastText.text = "";
-				lastMessagesInChat.lastText.count = 1;
+				return false;
 			}
-			return false;
-		}],
-		function(func, callback){callback(func());}, // mini function to call the functions of the array and pass their result to callback
-		function(reuslt){
+		],
+		function (func, callback) {
+			callback(func());
+		}, // mini function to call the functions of the array and pass their result to callback
+		function (reuslt) {
 			// reply if one of the spam filters are true
-			if(reuslt){
+			if(reuslt) {
 				if(noSpam.publicReply) bot.reply(message, noSpam.reply);
 				else bot.sendMessage(message.from.id, noSpam.reply);
 			}
