1ea6dd2f77f42ac9eea4e573b6849a46c5e4c0f8
Te Standby <testandby@gmail.com>
2016-03-03 14:18:03 +0000

    While writing req funcs

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as os from 'os'
 import * as extjs from 'external-ip'
+import * as Lz from 'lazy.js'
+import * as prmfy from 'promisify-node'
+import * as oReq from 'request'
 import * as tg from './lib/telegram.js'
 import * as wh from './lib/webhook.js'
 
 const O = Object
 const Prm = Promise
+const req = prmfy(oReq)
 
 function getOsHost() {
 	const host = os.hostname()
@@ -19,20 +23,28 @@ export default class Bot {
 	}
 
 	cfgr(cfg) {
+		const prmPz = [
+			'host',
+			'port',
+			'key',
+			'cert',
+			'ssl'
+		]
+
 		return new Prm((rsv, rjc)=> {
 			// TODO put addr getter in own module
 			new Prm((rsv2, rjc2)=> {
-				const cfgp = O.assign(O.seal({
+				const cfgp = Lz(cfg).pick(prmPz).map(Prm.resolve).defaults(cfg).defaults({
 					host: null,
 					port: 8443,
-				}), cfg)
+				})
 
 				if (!O.is(null, cfg.host)) {
 					rsv2(cfgp)
 				} else {
 					const osHost = getOsHost()
 
-					(osHost ? Prm.resolve(osHost) : new Prm((rsv3, rjc3)=>
+					osHost ? Prm.resolve(osHost) : new Prm((rsv3, rjc3)=>
 						extjs()((err, extHost)=>
 							// intended that it's `rjc`, not `rjc3`
 							err ? rjc(err) : rsv3(extHost)
@@ -42,16 +54,31 @@ export default class Bot {
 					)
 				}
 			}).then((cfgp)=>
-				rsv(O.assign(this, cfgp, {
-					addr: url.format(O.assign({
-						protocol: 'https',
-					}, cfgp)),
-				}))
+				rsv(O.assign(this, cfgp))
 			)
+		})
+	}
+
+	getHkAddr() {
+		return url.format(Lz(this).pick(['host', 'port']).assign({
+			protocol: 'https',
+			pathname: 'bot' + this.token,
+		}))
+	}
+
+	getMethAddr(meth) {
+		return url.format({
+			protocol: 'https',
+			host: 'api.telegram.org',
+			pathname:  [
+				'bot' + this.token,
+				meth,
+			].join('/'),
 		})
 	}
 }
 
+//TODO put in own module(s)
 class Chat {
 
 }
@@ -78,6 +105,7 @@ class Txtd extends Content {
 class Text extends Txtd {
 	{
 		parser: null,
+		preview: true,
 	}
 }
 
@@ -97,55 +125,4 @@ class Contact extends Content {
 
 class Loc extends Msg {
 
-}
-
-//Webhooks
-if (fs.existsSync(path.resolve(__dirname, "webhook.json"))){
-	webhook.setup(require("./webhook.json"));
-}
-
-// Load all existing botmodules
-var botmodules = {};
-// read all files from the botmodules folder and loop through them
-fs.readdirSync(path.resolve(__dirname, 'botmodules')).forEach(function(modulefilename){
-	var modulepath = path.resolve(__dirname, 'botmodules', modulefilename); //get the absolute path to the module
-	if(path.extname(modulepath) == '.js'){
-		var modulename = path.basename(modulepath, '.js'); // get the name of the module (the filename minus '.js')
-		botmodules[modulename] = require(modulepath);
-		console.log(modulename + " loaded");
-	}
-	// TODO: handle folders as botmodules
-});
-
-// Start the different Bots
-async.each(config.bots, function(botConfig){
-	new telegram(apikeys[botConfig.username], botConfig, webhook)
-	// set the on loaded function to initialize the botmodules
-	.on('loaded', function(){
-		var self = this;
-		// Initialize every module of the bot
-		async.each(Object.keys(this.config.modules), function(moduleName, done){
-			if(botmodules[moduleName] && typeof botmodules[moduleName].init == 'function'){
-				botmodules[moduleName].init(self, done);
-			}else{
-				done();
-			}
-		},
-		// When the initialization of the modules is finished, log the start
-		function(){
-			if(self.me.username.toUpperCase() != self.config.username.toUpperCase()) console.info("Warning: %s has a different username ('%s' expected, returned '%s')", self.config.username, self.config.username, self.me.username);
-			console.log('%s started', self.config.username);
-		});
-	})
-	// set the on message function to handle incoming messages
-	.on('message', function(msg){
-		var self = this;
-		// If a message is received, pass it to all activeated modules
-		async.each(Object.keys(this.config.modules), function(moduleName){
-			if(botmodules[moduleName] && typeof botmodules[moduleName].process == 'function'){
-				botmodules[moduleName].process(self, msg);
-			}
-		});
-	});
-});
 }
